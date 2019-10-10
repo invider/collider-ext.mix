7ecc7a3f264a48e15cad68a91ac879a6b0c633ca
Igor Khotin <khotin@gmx.com>
2019-10-10 20:39:35 +0300

    [+] configurable boot

--- fix/boot.js
+++ fix/boot.js
@@ -1,15 +1,81 @@
 'use strict'
 
-const base = '#151208'
-const content = '#a05020'
-const contentLow = '#402010'
+let base = '#151208'
+let content = '#a05020'
+let contentLow = '#402010'
+
+let hold = 1
+let fade = 0.5
+let wait = 0.5
+
+let time = 0
+let state = 'loading'
 
 module.exports = {
+
+    init: function() {
+        if (env.config.boot) {
+            const bt = env.config.boot
+            hold = bt.hold || hold
+            fade = bt.fade || fade
+            wait = bt.wait || wait
+            base = bt.base || base
+            content = bt.content || content
+            contentLow = bt.contentLow || contentLow
+        }
+    },
+
+    evo: function(dt) {
+        time += dt
+
+        switch (state) {
+        case 'loading':
+            if (env._started) {
+                state = 'holding'
+            }
+            break;
+
+        case 'holding':
+            if (time >= hold) {
+                time = 0
+                state = 'fade'
+            }
+            break;
+
+        case 'fade':
+            if (time >= fade) {
+                time = 0
+                state = 'wait'
+            }
+            break;
+
+        case 'wait':
+            if (time >= wait) {
+                state = 'self-destruct'
+            }
+            break;
+
+        case 'self-destruct':
+            kill(this)
+            break;
+        }
+    },
+
     draw: function() {
         // clear the screen
         ctx.fillStyle = base
         ctx.fillRect(0, 0, canvas.width, canvas.height)
-        
+
+        fill('#ffffff')
+        text('state: ' + state, ctx.width/2, 100)
+
+        if (state === 'wait' || state === 'self-destruct') return
+
+        ctx.save()
+        if (state === 'fade') {
+            ctx.globalAlpha = 1 - time/fade
+        }
+
         let loaded = this._.___.res._loaded
         let included = this._.___.res._included
         let percent = Math.round((loaded/included) * 100)
@@ -39,5 +105,9 @@ module.exports = {
         ctx.font = '20px zekton'
         ctx.fillStyle = content
         ctx.fillText('Powered by Collider.JAM', ctx.width/2, ctx.height - 50)
+
+
+        ctx.restore()
     }
+
 }
